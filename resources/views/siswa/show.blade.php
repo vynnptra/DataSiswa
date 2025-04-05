98fa96dea146efe78dacd8ab66dd339f1d375031
vynnptra <mryu.uiiiii@gmail.com>
2025-04-05 17:09:56 +0700

    add Hobby unitTest

--- resources/views/siswa/show.blade.php
+++ resources/views/siswa/show.blade.php
@@ -1,41 +1,3 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container mt-5">
-        <div class="card shadow-lg p-4">
-            <div class="row">
-                <div class="col-md-8">
-                    <h2 class="fw-bold">{{ $siswas->nama }}</h2>
-                    <p class="text-muted">NISN: {{ $siswas->nisn->nisn }}</p>
-                    <p><strong>Telepon:</strong> {{ $siswas->phoneNumbers->pluck('phone_number')->implode(', ') }} </p>
-                    <p><strong>Hobbies:</strong> {{ $siswas->hobbies->pluck('name')->implode(', ') }}</p>
-                    <p><strong>Alamat:</strong> Jl. Contoh No. 123, Kota ABC</p>
-                    <div class="mt-3 d-flex gap-2">
-                        <a href="{{ route('siswa.edit', $siswas->id) }}" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Edit</a>
-
-                        <form action="{{ route('siswa.destroy', $siswas->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-danger">
-                                <i class="bi bi-trash-fill"></i> Hapus
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-    <script src="ttps://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html> --}}
-
 
 @extends('layouts.app')
 
@@ -45,7 +7,7 @@
         {{ route('siswa.index') }}
     </x-slot:redirect>
 
-    {{-- <h3 class="text-2xl font-semibold text-slate-900 dark:text-white my-7">{{ $siswa->name }}</h3> --}}
+
     <div class="my-7">
         
             <h2 class="text-xl font-bold text-slate-800 dark:text-white mb-2">{{ $siswa->nama }}</h2>
--- tests/Unit/HobbyTest.php
+++ tests/Unit/HobbyTest.php
@@ -2,15 +2,62 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Hobby;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class HobbyTest extends TestCase
 {
+
+    use RefreshDatabase;
     /**
      * A basic unit test example.
      */
-    public function test_example(): void
-    {
-        $this->assertTrue(true);
-    }
+
+     /** @test */
+     public function user_can_create_hobby(){
+        $hobby = Hobby::create([
+            'name' => 'test',
+        ]);
+
+        $this->assertInstanceOf(Hobby::class, $hobby);
+        $this->assertEquals('test', $hobby->name);
+     }
+
+     /** @test */
+     public function user_can_update_hobby(){
+        $hobby = Hobby::create([
+            'name' => "test1",
+        ]);
+        
+        $hobby->update([
+            'name' => "test2",
+        ]);
+
+        $this->assertInstanceOf(Hobby::class, $hobby);
+        $this->assertEquals('test2', $hobby->name);
+     }
+
+     /** @test */
+     public function user_can_show_detail_hobby(){
+        $hobby = Hobby::create([
+            'name' => 'test',
+        ]);
+
+        $detailHobby = Hobby::find($hobby->id);
+
+        $this->assertInstanceOf(Hobby::class, $detailHobby);
+        $this->assertEquals('test', $detailHobby->name);
+     }
+
+     /** @test */
+     public function user_can_delete_hobby() {
+        $hobby = Hobby::create([
+            'name' => 'test',
+        ]);
+        $this->assertNotNull($hobby);
+        $hobby->delete();
+     }
+
+
 }
