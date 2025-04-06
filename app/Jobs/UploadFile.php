a7b14892ea11fe9e04eb8fde47dc6db86a20002c
vynnptra <mryu.uiiiii@gmail.com>
2025-04-06 20:42:44 +0700

    upload file with queue

---
+++ app/Jobs/UploadFile.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Hobby;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Storage;
+
+class UploadFile implements ShouldQueue
+{
+    use Queueable;
+
+    protected $tempPath;
+    protected $fileName;
+    protected $hobbyId;
+
+    public function __construct($tempPath, $fileName, $hobbyId)
+    {
+        $this->tempPath = $tempPath;
+        $this->fileName = $fileName;
+        $this->hobbyId = $hobbyId;
+    }
+
+    public function handle(): void
+    {
+        $sourcePath = storage_path('app/' . $this->tempPath);
+        $destination = 'hobbies/' . $this->fileName;
+
+        // Streaming: buka file lalu kirim ke lokasi tujuan
+        $stream = fopen($sourcePath, 'r');
+        Storage::put($destination, $stream);
+        fclose($stream);
+
+        // Update model
+        $hobby = Hobby::find($this->hobbyId);
+        if ($hobby) {
+            $hobby->update([
+                'file' => $destination,
+            ]);
+        }
+
+        // Hapus file sementara
+        Storage::delete($this->tempPath);
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HobbyController;
 use App\Http\Controllers\SiswaController;
+use App\Jobs\Test;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,3 +12,8 @@ Route::get('/', function () {
 Route::resource('/hobby', HobbyController::class );
 
 Route::resource('/siswa', SiswaController::class);
+
+Route::get('test', function () {
+    Test::dispatch();
+    return 'test';
+});
