0390aa9a498a60cd0354289806c521d9c1b088ca
vynnptra <mryu.uiiiii@gmail.com>
2025-04-07 11:51:41 +0700

    change required to nullable

--- database/migrations/2025_04_07_003203_add_role_to_users.php
+++ database/migrations/2025_04_07_003203_add_role_to_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['superadmin', 'admin', 'user'])->default('user');
+            $table->enum('role', ['superadmin', 'admin', 'user'])->default('user')->nullable();
         });
     }
 
